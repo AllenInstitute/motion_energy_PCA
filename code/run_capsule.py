412715a107a93df16e1a80c6fdb581bcc02c53fe
yavorska-iryna <iryna.yavorska@alleninstitute.org>
2025-02-26 21:37:06 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -5,7 +5,7 @@ import time  # Added for timing
 from PCAgenerator import PCAgenerator
 from pathlib import Path
 
-DATA_PATH = Path("*/data/")
+DATA_PATH = Path("/root/capsule/data/")
 zarr_paths = utils.find_files(root_dir = DATA_PATH, endswith='zarr')
 npz_paths = utils.find_files(root_dir = DATA_PATH, endswith='.npz', return_dir=False)
 print(len(zarr_paths))
