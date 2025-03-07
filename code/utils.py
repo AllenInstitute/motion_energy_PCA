4159a61198c34d69327afa9f9019ea3e6b11c85f
yavorska-iryna <iryna.yavorska@alleninstitute.org>
2025-03-07 22:21:25 +0000

    removing npz loading function

--- code/utils.py
+++ code/utils.py
@@ -208,21 +208,3 @@ def object_to_dict(obj):
 
 
 
-def load_npz_file(npz_file_path: str) -> dict:
-    """
-    Loads a NumPy `.npz` file and returns its contents.
-
-    Args:
-        npz_file_path (str): Path to the `.npz` file.
-
-    Returns:
-        dict: Dictionary containing NumPy arrays from the NPZ file.
-
-    Raises:
-        FileNotFoundError: If the specified NPZ file does not exist.
-        ValueError: If the NPZ file is empty.
-    """
-    
-    npz_data = np.load(npz_file_path)
-    return npz_data
-
