5fa2c5538c10ee4016140917319b26ae08bcdaba
yavorska-iryna <iryna.yavorska@alleninstitute.org>
2025-03-11 23:35:28 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -7,7 +7,7 @@ from pathlib import Path
 import numpy as np
 
 DATA_PATH = utils.get_data_path(pipeline=True)
-zarr_paths = list(DATA_PATH.glob("*/*/motion_energy_frames.zarr"))
+zarr_paths = list(DATA_PATH.glob("*/motion_energy_frames.zarr"))
 print(f"paths to zarr files: {zarr_paths}")
 def run():
     #for zarr_path, npz_path in zip(zarr_paths[:1], npz_paths[:1]):
